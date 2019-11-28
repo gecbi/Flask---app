228f7784a8c83c5b211bf2f1d0e345c33bbc7f95
https://github.com/gecbi/web-app---flask.git <georgi.kostov@hotmail.com>
2019-11-29 00:41:05 +0200

    finished #views.py (Create, View, Update and Delete) marked Htmltmplates (blog_post.html, create.html)

--- mainfolder/blog_posts/views.py
+++ mainfolder/blog_posts/views.py
@@ -16,8 +16,61 @@ def create_post ():
     if form.validate_on_submit():
 
         blog_post = BlogPost(title=form.title.data, text=form.text.data, user_id=current_user.id)
-        
+
+        db.session.add(blog_post)
+        db.session.commit()
+        flash ('Blog Post Created')
+        return redirect(url_for('core.index'))
+
+    return render_template('create_post.html', form =form)
 
 # BLOG POST (VIEW)
+@blog_post.route('/<int:blog_post_id>')
+def blog_post(blog_post_id):
+    blog_post= BlogPost.query.get_or_404(blog_post_id)
+    return render_template('blog_post.html', title=blog_post.title,
+    date=blog_post.date, post=blog_post)
+
 # UPDATE
+@blog_post.route('/<int:blog_post_id/update', methods= ['GET','POST'])
+@login_required
+def update(blog_post_id):
+
+    blog_post = BlogPost.query.get_or_404(blog_post_id)
+
+    if blog_post.author != current_user:
+        aborth(403)
+
+    form = BlogPostForm()
+
+    if form.validate_on_submit():
+        blog_post.title = form.title.data
+        blog_post.text = form.text.data
+
+        flash('Blog Post Updated')
+        return redirect(url_for('blog_post.blog_post'), blog_post_id = blog_post_id)
+
+    elif request.method = 'GET':
+
+        form.text.data = blog_post.text
+        form.title.data = blog_post.title
+
+    return render_template('create_post.html', title ='Updating', form =form)
+
 # DELETE
+@blog_post.route('/<int:blog_post_id/delete', methods = ['GET', 'POST'])
+@login_required
+
+def delete_post(blog_post_id):
+
+    blog_post = BlogPost.query.get_or_404(blog_post_id)
+
+    if blog_post.author != current_user:
+        aborth(403)
+
+    db.session.delete(blog_post)
+    db.session.commit()
+    flash('Blog Post Deleted')
+
+    return redirect(url_for('core.index'))
+    
